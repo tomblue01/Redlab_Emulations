443c8091d007cdddc6390a45f1d78eb470642a44
tomblue01 <tom.blue01@gmail.com>
2025-10-16 12:23:51 -0500

    Update download script for Atomics library

--- attackchain.ps1
+++ attackchain.ps1
@@ -274,9 +274,9 @@ function Check-Prerequisites {
     if (-NOT (Test-Path "C:\AtomicRedTeam\atomics")) {
         Write-Log -Level WARN "Atomic Red Team test library not found. Downloading..."
         try {
-            # Need to re-run installation script to get Install-AtomicRedTeam function
-            Write-Log "Downloading installation script to access Install-AtomicRedTeam function..."
-            IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -GetAtomics -Force
+            # Use the separate install-atomicsfolder.ps1 script for just the atomics
+            Write-Log "Downloading atomics folder using Install-AtomicsFolder function..."
+            IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicsfolder.ps1' -UseBasicParsing); Install-AtomicsFolder -Force
             
             # Verify download succeeded
             if (-NOT (Test-Path "C:\AtomicRedTeam\atomics")) {
@@ -287,7 +287,7 @@ function Check-Prerequisites {
         } catch {
             $errorMsg = $_.Exception.Message
             Write-Log -Level ERROR "Failed to download the Atomics library: $errorMsg"
-            Write-Log -Level ERROR "Please manually run: IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -GetAtomics"
+            Write-Log -Level ERROR "Please manually run: IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicsfolder.ps1' -UseBasicParsing); Install-AtomicsFolder"
             throw "Atomics download failed"
         }
     } else {
